ff636cbc5aaec954bfd4e5af352d8c80a58cb43e
Hystabec <edward14577777@gmail.com>
2025-04-03 18:15:25 +0100

    removed std::cout for testing left in simCore()

--- gossipSim/src/simulationCore.cpp
+++ gossipSim/src/simulationCore.cpp
@@ -17,13 +17,10 @@ namespace GS {
 		{
 			npc::NPC* curNPC = new npc::NPC(node_NPC.attribute("name").as_string());
 
-			std::cout << node_NPC.attribute("name").as_string() << "\n";
-
 			//parseNodeFiles - generate relationships
 			for (pugi::xml_node node_relation : node_NPC.child("relationships"))
 			{
 				curNPC->addRelation(node_relation.attribute("npc").as_string(), node_relation.attribute("value").as_int());
-				std::cout << node_relation.attribute("npc").as_string() << " " << node_relation.attribute("value").as_int() << "\n";
 			}
 
 			m_npcVec.push_back(curNPC);
