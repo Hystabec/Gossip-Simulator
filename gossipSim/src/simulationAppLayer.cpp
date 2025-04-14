49037f8f29c81556e9d7a61b70952d1886b3dd38
Hystabec <edward14577777@gmail.com>
2025-04-14 17:34:59 +0100

    added some test circle rendering

--- gossipSim/src/simulationAppLayer.cpp
+++ gossipSim/src/simulationAppLayer.cpp
@@ -15,12 +15,13 @@
 	//}
 
 SimLayer::SimLayer()
+	: m_camController(1280.0f / 720.0f)
 {
 }
 
 void SimLayer::attach()
 {
-	
+	m_circleTexture = daedalusCore::graphics::Texture2D::create("resources/circle.png");
 }
 
 void SimLayer::detach()
@@ -29,8 +30,15 @@ void SimLayer::detach()
 
 void SimLayer::update(const daedalusCore::application::DeltaTime& dt)
 {
+	m_camController.update(dt);
+
 	daedalusCore::graphics::RenderCommands::setClearColour(daedalusCore::utils::colour_vec4_to_normalized_vec4({ 36,36,36,255 }));
 	daedalusCore::graphics::RenderCommands::clear();
+
+	daedalusCore::graphics::Renderer2D::begin(m_camController.getCamera());
+	daedalusCore::graphics::Renderer2D::drawQuad({ { -0.5f, 0 }, { 0.5f, 0.5f }, m_circleTexture, { 0.3f, 0.8f, 0.2f, 1.0f } });
+	daedalusCore::graphics::Renderer2D::drawQuad({ { 0.5f, 0 }, { 0.5f, 0.5f }, m_circleTexture, { 0.8f, 0.3f, 0.2f, 1.0f } });
+	daedalusCore::graphics::Renderer2D::end();
 }
 
 void SimLayer::imGuiRender()
@@ -39,4 +47,5 @@ void SimLayer::imGuiRender()
 
 void SimLayer::onEvent(daedalusCore::event::Event& e)
 {
+	m_camController.onEvent(e);
 }
--- gossipSim/src/simulationAppLayer.h
+++ gossipSim/src/simulationAppLayer.h
@@ -11,4 +11,8 @@ public:
 	void update(const daedalusCore::application::DeltaTime& dt) override;
 	void imGuiRender() override;
 	void onEvent(daedalusCore::event::Event& e) override;
+
+private:
+	daedalusCore::graphics::OrthographicCameraController m_camController;
+	daedalusCore::shr_ptr<daedalusCore::graphics::Texture2D> m_circleTexture;
 };
